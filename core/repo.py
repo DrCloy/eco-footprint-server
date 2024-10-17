f705dfedbb8154cb56e96c4290034b7a0f7a00fe
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2024-10-17 22:37:34 +0900

    Update repository abstract class

--- core/repo.py
+++ core/repo.py
@@ -1,1 +1,20 @@
 from abc import ABC, abstractmethod
+from core.model import UserData, DonationData, CampainData
+
+
+class UserRepository(ABC):
+    @abstractmethod
+    def get_user(self, user_id: int) -> UserData:
+        pass
+
+
+class DonationRepository(ABC):
+    @abstractmethod
+    def get_donation(self, donation_id: int) -> DonationData:
+        pass
+
+
+class CampainRepository(ABC):
+    @abstractmethod
+    def get_campain(self, campain_id: int) -> CampainData:
+        pass
