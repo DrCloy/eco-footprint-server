794b57edcc8da9c90d97f3a15a6bbd939aa65e74
MinGyo Kim <110979416+DrCloy@users.noreply.github.com>
2024-10-19 09:52:11 +0900

    Add router for user data

--- routers/userRouter.py
+++ routers/userRouter.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
+from core.repo import UserRepository
+from repo.user_test import UserTestRepo
 
 userRouter = APIRouter()
+
+userRepo: UserRepository = UserTestRepo()
+
+
+userRouter.add_api_route('/{user_id}', userRepo.get_user, methods=['GET'])
