0604c666c70365e6cbf1f79d1ada46819c1aeff0
DrCloy <alsry0712@gmail.com>
2024-10-30 06:18:35 +0000

    Update model

--- core/model.py
+++ core/model.py
@@ -2,26 +2,117 @@ from pydantic import BaseModel, Field
 from typing import Union, Optional
 
 
-class UserData(BaseModel):
+class ItemState(enumerate):
+    UNDEFINED = -1
+    PENDING = 0
+    ACTIVE = 1
+    INACTIVE = 2
+    FINISHED = 3
+    FAILED = 4
+
+
+class UserItemMeta(BaseModel):
+    id: str
+    username: str
+    thumbnailId: Optional[str] = None
+
+
+class DonationItmeMeta(BaseModel):
+    id: Optional[str] = None
+    name: str
+    currentPoint: int = Field(..., ge=0)
+    totalPoint: int = Field(..., ge=0)
+    thumbnailId: Optional[str] = None
+
+
+class CouponItemMeta(BaseModel):
+    id: Optional[str] = None
+    itemName: str
+    brandName: str
+    thumbnailId: Optional[str] = None
+    expiredAt: str
+
+
+class ChallengeItemMeta(BaseModel):
+    id: Optional[str] = None
+    name: str
+    totalParticipants: int = Field(..., ge=0)
+    currentParticipants: int
+    dateEnd: str
+
+
+class ChallengeRecordItem(BaseModel):
+    userId: str
+    imageId: str
+
+
+class RewardItemMeta(BaseModel):
+    id: str
+    itemName: str
+    brandName: str
+    itemType: str
+    point: int = Field(..., ge=0)
+    thumbnailId: str
+
+
+class UserItem(UserItemMeta):
     id: str
     username: str
     point: int = Field(..., ge=0)
-    giftIds: list[str] = []
-    DonationIds: list[str] = []
-    campainIds: list[str] = []
-    thumbnailURL: Optional[str] = None
+    couponList: list[CouponItemMeta] = []
+    challengeList: list[ChallengeItemMeta] = []
+    thumbnailId: str = ''
 
 
-class DonationData(BaseModel):
+class DonationItem(DonationItmeMeta):
     id: str
     name: str
+    currentPoint: int = Field(..., ge=0)
     totalPoint: int = Field(..., ge=0)
-    participants: Union[list[UserData], list[str]]
     description: str
+    participants: list[str] = []
+    thumbnailId: Optional[str] = None
+    state: ItemState = ItemState.UNDEFINED
 
 
-class CampainData(BaseModel):
+class CouponItem(CouponItemMeta):
+    id: str
+    itemName: str
+    brandName: str
+    description: str
+    thumbnailId: Optional[str] = None
+    couponId: str
+    expiredAt: str
+
+
+class ChallengeItem(ChallengeItemMeta):
     id: str
     name: str
+    totalParticipants: int = Field(..., ge=0)
+    currentParticipants: int
+    createdAt: str
+    participants: list[UserItemMeta] = []
+    participantRecords: list[ChallengeRecordItem] = []
+    dateStart: str
+    dateEnd: str
     description: str
-    participants: Union[list[UserData], list[str]]
+    state: ItemState = ItemState.UNDEFINED
+
+
+class RewardItem(RewardItemMeta):
+    id: str
+    itemName: str
+    brandName: str
+    itemType: str
+    description: str
+    imageId: str
+    thumbnailId: str
+    price: int = Field(..., ge=0)
+    provider: str
+
+
+class FileData(BaseModel):
+    id: str
+    name: str
+    size: int
+    contentType: str
