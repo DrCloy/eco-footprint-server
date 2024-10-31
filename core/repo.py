6d7aab46da09c7a0bc8dda6ecbd724e094a50105
DrCloy <alsry0712@gmail.com>
2024-10-31 15:55:46 +0000

    Update repository abstract class

--- core/repo.py
+++ core/repo.py
@@ -1,36 +1,124 @@
+from fastapi import UploadFile
+
 from abc import ABC, abstractmethod
-from core.model import UserData, DonationData, CampainData
+from core.model import *
 
 
 class UserRepository(ABC):
     @abstractmethod
-    def getUserInfo(self, userId: str) -> UserData:
+    def createUser(self, userItem: UserItem) -> UserItem:
         pass
 
     @abstractmethod
-    def createUser(self, user: UserData) -> UserData:
+    def getUser(self, userId: str) -> UserItem:
         pass
 
     @abstractmethod
-    def updateUser(self, user: UserData) -> UserData:
+    def updateUser(self, userItem: UserItem) -> UserItem:
+        pass
+
+    @abstractmethod
+    def deleteUser(self, userId: str) -> bool:
         pass
 
 
 class DonationRepository(ABC):
     @abstractmethod
-    def getAllDonations(self) -> list[DonationData]:
+    def createDonation(self, donationItem: DonationItem) -> DonationItem:
         pass
 
     @abstractmethod
-    def getDonation(self, donationId: int) -> DonationData:
+    def getAllDonations(self) -> list[DonationItemMeta]:
         pass
 
     @abstractmethod
-    def createDonation(self, donation: DonationData) -> DonationData:
+    def getDonation(self, donationId: str) -> DonationItem:
+        pass
+
+    @abstractmethod
+    def updateDonation(self, donationItem: DonationItem) -> DonationItem:
+        pass
+
+    @abstractmethod
+    def deleteDonation(self, donationId: str) -> bool:
         pass
 
 
-class CampainRepository(ABC):
+class CouponRepository(ABC):
     @abstractmethod
-    def getCampain(self, campainId: int) -> CampainData:
+    def createCoupon(self, couponItem: CouponItem) -> CouponItem:
+        pass
+
+    @abstractmethod
+    def getCoupon(self, couponId: str) -> CouponItem:
+        pass
+
+    @abstractmethod
+    def updateCoupon(self, couponItem: CouponItem) -> CouponItem:
+        pass
+
+    @abstractmethod
+    def deleteCoupon(self, couponId: str) -> bool:
+        pass
+
+
+class ChallengeRepository(ABC):
+    @abstractmethod
+    def createChallenge(self, challengeItem: ChallengeItem) -> ChallengeItem:
+        pass
+
+    @abstractmethod
+    def getAllChallenges(self) -> list[ChallengeItemMeta]:
+        pass
+
+    @abstractmethod
+    def getChallenge(self, challengeId: str) -> ChallengeItem:
+        pass
+
+    @abstractmethod
+    def updateChallenge(self, challengeItem: ChallengeItem) -> ChallengeItem:
+        pass
+
+    @abstractmethod
+    def deleteChallenge(self, challengeId: str) -> bool:
+        pass
+
+
+class RewardRepository(ABC):
+    @abstractmethod
+    def createReward(self, rewardItem: RewardItem) -> RewardItem:
+        pass
+
+    @abstractmethod
+    def getAllRewards(self) -> list[RewardItemMeta]:
+        pass
+
+    @abstractmethod
+    def getReward(self, rewardId: str) -> RewardItem:
+        pass
+
+    @abstractmethod
+    def updateReward(self, rewardItem: RewardItem) -> RewardItem:
+        pass
+
+    @abstractmethod
+    def deleteReward(self, rewardId: str) -> bool:
+        pass
+
+
+class FileRepository(ABC):
+    @abstractmethod
+    def createFile(self, file: UploadFile) -> FileData:
+        pass
+
+    @abstractmethod
+    def getFile(self, fileId: str) -> FileData:
+        pass
+
+    @abstractmethod
+    def updateFile(self, file: UploadFile, fileData: FileData) -> FileData:
+        pass
+
+    @abstractmethod
+    def deleteFile(self, fileId: str) -> bool:
         pass
