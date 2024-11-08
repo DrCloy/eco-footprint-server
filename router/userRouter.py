5193a7e5cb3dec814dbec775c6ce209bb595a6a3
DrCloy <alsry0712@gmail.com>
2024-11-08 05:56:21 +0000

    Update user router

--- router/userRouter.py
+++ router/userRouter.py
@@ -1,22 +1,82 @@
-from fastapi import APIRouter
+import os
+
+from fastapi import APIRouter, Request, HTTPException
 from pymongo.database import Database
 
-from core.model import UserData
+from core.model import UserItem
 from core.repo import UserRepository
 
 
 class UserRouter(APIRouter):
-    def __init__(self, db: Database, repo: UserRepository):
-        super().__init__(prefix="/user", tags=["User"])
-        self.db = db
-        self.collection = self.db.users
-        self.repo = repo
+    def __init__(self, userRepo: UserRepository):
+        super().__init__()
+        self._userRepo = userRepo
 
-        self.add_api_route("/register", self.register_user, methods=["POST"])
-        self.add_api_route("/{userId}", self.get_user, methods=["GET"])
+        self.add_api_route(path="/register", endpoint=self.register, methods=["POST"])
+        self.add_api_route(path="/profile/{userId}", endpoint=self.getProfile, methods=["GET"])
+        self.add_api_route(path="/profile", endpoint=self.updateProfile, methods=["Put"])
+        self.add_api_route(path="/delete/{userId}", endpoint=self.deleteUser, methods=["DELETE"])
 
-    def get_user(self, userId: str):
-        return self.repo.getUserInfo(userId)
+    def register(self, userItem: UserItem, request: Request):
+        if request.state.auth is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-    def register_user(self, user: UserData):
-        return self.repo.createUser(user)
+        if request.state.auth["role"] == "test":
+            userId = request.state.auth["userId"]
+        else:
+            userId = request.state.auth["sub"]
+            if self.getProfile(userId, request):
+                raise HTTPException(status_code=409, detail="User already exists")
+
+        userItem.userId = userId
+        user = self._userRepo.createUser(userItem)
+
+        return user
+
+    def getProfile(self, userId: str, request: Request):
+        if request.state.auth is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        if request.state.auth["role"] == "test":
+            pass
+        else:
+            if request.state.auth["sub"] != userId:
+                raise HTTPException(status_code=401, detail="Unauthorized")
+
+        user = self._userRepo.getUser(userId)
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        return user
+
+    def updateProfile(self, userItem: UserItem, request: Request):
+        if request.state.auth is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        if request.state.auth["role"] == "test":
+            pass
+        else:
+            if not self.getProfile(request.state.auth["sub"], request):
+                raise HTTPException(status_code=404, detail="User not found")
+        userItem.userId = request.state.auth["sub"]
+
+        user = self._userRepo.updateUser(userItem)
+
+        return user
+
+    def deleteUser(self, userId: str, request: Request):
+        if request.state.auth is None:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        if request.state.auth["role"] == "test":
+            pass
+        else:
+            if not self.getProfile(request.state.auth["sub"], request):
+                raise HTTPException(status_code=404, detail="User not found")
+            if request.state.auth["sub"] != userId:
+                raise HTTPException(status_code=401, detail="Unauthorized")
+
+        result = self._userRepo.deleteUser(userId)
+
+        return result
