8b947b73f409a0ffcbe6c8ffd228502de983ab23
DrCloy <alsry0712@gmail.com>
2024-11-24 12:08:32 +0000

    Add docstring and refactor

--- repo/rewardMongo.py
+++ repo/rewardMongo.py
@@ -8,12 +8,27 @@ from core.repo import RewardRepository
 
 
 class RewardMongoRepo(RewardRepository):
+    """
+    Implementation of RewardRepository using MongoDB
+    """
+
     def __init__(self, db: Database):
         super().__init__()
         self._db = db
         self._collection = self._db["rewards"]
 
     def createReward(self, rewardItem: RewardItem) -> RewardItem:
+        """
+        Create a new reward item
+
+        Args:
+            rewardItem (RewardItem): Reward item to create
+
+        Raises:
+            HTTPException: If failed to create reward
+
+        Returns:
+            RewardItem: Created reward item"""
         if not rewardItem.id:
             rewardItem.id = str(ObjectId())
 
@@ -26,17 +41,44 @@ class RewardMongoRepo(RewardRepository):
             raise HTTPException(status_code=500, detail="Failed to create reward")
 
     def getAllRewards(self) -> list[RewardItemMeta]:
+        """
+        Get all reward items
+
+        Returns:
+            list[RewardItemMeta]: List of reward items
+        """
         rewards = self._collection.find()
         return [RewardItemMeta(**reward) for reward in rewards]
 
     def getReward(self, rewardId: str) -> RewardItem:
+        """
+        Get a reward item by ID
+
+        Args:
+            rewardId (str): Reward ID
+
+        Returns:
+            RewardItem: Reward item, or None if not found
+        """
         reward = self._collection.find_one({"id": rewardId})
         if reward:
             return RewardItem(**reward)
         else:
-            raise HTTPException(status_code=404, detail="Reward not found")
+            return None
 
     def updateReward(self, rewardItem: RewardItem) -> RewardItem:
+        """
+        Update a reward item
+
+        Args:
+            rewardItem (RewardItem): Reward item to update
+
+        Raises:
+            HTTPException: If failed to update reward
+
+        Returns:
+            RewardItem: Updated reward item
+        """
         self._collection.update_one({"id": rewardItem.id}, {"$set": rewardItem.model_dump()})
 
         reward = self._collection.find_one({"id": rewardItem.id})
@@ -46,5 +88,14 @@ class RewardMongoRepo(RewardRepository):
             raise HTTPException(status_code=500, detail="Failed to update reward")
 
     def deleteReward(self, rewardId: str) -> bool:
+        """
+        Delete a reward item by ID
+
+        Args:
+            rewardId (str): Reward ID
+
+        Returns:
+            bool: True if deleted, False if not found
+        """
         result = self._collection.delete_one({"id": rewardId})
         return result.deleted_count > 0
