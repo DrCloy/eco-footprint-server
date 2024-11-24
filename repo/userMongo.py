be45f470783eb41b2bf6c766d9e573b5444c0b90
DrCloy <alsry0712@gmail.com>
2024-11-24 11:57:28 +0000

    Add docstring

--- repo/userMongo.py
+++ repo/userMongo.py
@@ -7,12 +7,30 @@ from core.repo import UserRepository
 
 
 class UserMongoRepo(UserRepository):
+    """
+    Implementation of UserRepository using MongoDB
+
+
+    """
+
     def __init__(self, db: Database):
         super().__init__()
         self._db = db
         self._collection = db["users"]
 
     def createUser(self, userItem: UserItem) -> UserItem:
+        """
+        Create a new user
+
+        Args:
+            userItem (UserItem): UserItem object
+
+        Raises:
+            HTTPException: If failed to create user
+
+        Returns:
+            UserItem: UserItem object
+        """
         self._collection.insert_one(userItem.model_dump())
 
         user = self._collection.find_one({"id": userItem.id})
@@ -22,6 +40,15 @@ class UserMongoRepo(UserRepository):
             raise HTTPException(status_code=500, detail="Failed to create user")
 
     def getUser(self, userId: str) -> UserItem:
+        """
+        Get a user by id
+
+        Args:
+            userId (str): User id
+
+        Returns:
+            UserItem: UserItem object
+        """
         user = self._collection.find_one({"id": userId})
         if user:
             return UserItem(**user)
@@ -29,6 +56,18 @@ class UserMongoRepo(UserRepository):
             return None
 
     def updateUser(self, userItem: UserItem) -> UserItem:
+        """
+        Update a user
+
+        Args:
+            userItem (UserItem): UserItem object
+
+        Raises:
+            HTTPException: If failed to update user
+
+        Returns:
+            UserItem: UserItem object
+        """
         self._collection.update_one({"id": userItem.id}, {"$set": userItem.model_dump()})
 
         user = self._collection.find_one({"id": userItem.id})
@@ -38,5 +77,14 @@ class UserMongoRepo(UserRepository):
             raise HTTPException(status_code=500, detail="Failed to update user")
 
     def deleteUser(self, userId: str) -> bool:
+        """
+        Delete a user by id
+
+        Args:
+            userId (str): User id
+
+        Returns:
+            bool: True if user is deleted, False otherwise
+        """
         result = self._collection.delete_one({"id": userId})
         return result.deleted_count > 0
