1723c04cb94f56a8b315d11fbae983dd7d79fa50
DrCloy <alsry0712@gmail.com>
2024-11-24 12:15:33 +0000

    Add challenge repository

---
+++ repo/challengeMongo.py
@@ -0,0 +1,100 @@
+from bson import ObjectId
+
+from fastapi import HTTPException
+
+from pymongo.database import Database
+
+from core.model import ChallengeItemMeta, ChallengeItem
+from core.repo import ChallengeRepository
+
+
+class ChallengeMongoRepo(ChallengeRepository):
+    """
+    Implementation of ChallengeRepository using MongoDB
+    """
+
+    def __init__(self, db: Database):
+        super().__init__()
+        self._db = db
+        self._collection = self._db["challenges"]
+
+    def createChallenge(self, challengeItem: ChallengeItem) -> ChallengeItem:
+        """
+        Create a new challenge
+
+        Args:
+            challengeItem (ChallengeItem): ChallengeItem object
+
+        Raises:
+            HTTPException: If failed to create challenge
+
+        Returns:
+            ChallengeItem: Created ChallengeItem object
+        """
+        self._collection.insert_one(challengeItem.model_dump())
+
+        challenge = self._collection.find_one({"id": challengeItem.id})
+        if challenge:
+            return ChallengeItem(**challenge)
+        else:
+            raise HTTPException(status_code=500, detail="Failed to create challenge")
+
+    def getAllChallenges(self) -> list[ChallengeItemMeta]:
+        """
+        Get all challenges
+
+        Returns:
+            list[ChallengeItemMeta]: List of ChallengeItemMeta objects
+        """
+        challenges = self._collection.find()
+        return [ChallengeItemMeta(**challenge) for challenge in challenges]
+
+    def getChallenge(self, challengeId: str) -> ChallengeItem:
+        """
+        Get a challenge by id
+
+        Args:
+            challengeId (str): Challenge id
+
+        Returns:
+            ChallengeItem: ChallengeItem object if found, None otherwise
+        """
+        challenge = self._collection.find_one({"id": challengeId})
+        if challenge:
+            return ChallengeItem(**challenge)
+        else:
+            return None
+
+    def updateChallenge(self, challengeItem: ChallengeItem) -> ChallengeItem:
+        """
+        Update a challenge
+
+        Args:
+            challengeItem (ChallengeItem): ChallengeItem object
+
+        Raises:
+            HTTPException: If failed to update challenge
+
+        Returns:
+            ChallengeItem: Updated ChallengeItem object
+        """
+        self._collection.update_one({"id": challengeItem.id}, {"$set": challengeItem.model_dump()})
+
+        challenge = self._collection.find_one({"id": challengeItem.id})
+        if challengeItem == challenge:
+            return ChallengeItem(**challenge)
+        else:
+            raise HTTPException(status_code=500, detail="Failed to update challenge")
+
+    def deleteChallenge(self, challengeId: str) -> bool:
+        """
+        Delete a challenge by id
+
+        Args:
+            challengeId (str): Challenge id
+
+        Returns:
+            bool: True if challenge is deleted, False otherwise
+        """
+        result = self._collection.delete_one({"id": challengeId})
+        return result.deleted_count > 0
