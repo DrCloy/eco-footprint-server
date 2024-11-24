539d12028f1e2d1f9ccaa0084f939f3bcb69c669
DrCloy <alsry0712@gmail.com>
2024-11-24 12:10:26 +0000

    Add docstring and Refactor

--- repo/couponMongo.py
+++ repo/couponMongo.py
@@ -8,12 +8,28 @@ from core.repo import CouponRepository
 
 
 class CouponMongoRepo(CouponRepository):
+    """
+    Implementation of CouponRepository using MongoDB
+    """
+
     def __init__(self, db: Database):
         super().__init__()
         self._db = db
         self._collection = db["coupons"]
 
     def createCoupon(self, couponItem: CouponItem) -> CouponItem:
+        """
+        Create a new coupon item
+
+        Args:
+            couponItem (CouponItem): Coupon item to be created
+
+        Raises:
+            HTTPException: Failed to create coupon
+
+        Returns:
+            CouponItem: Created coupon item
+        """
         if not couponItem.id:
             couponItem.id = str(ObjectId())
         self._collection.insert_one(couponItem.model_dump())
@@ -25,13 +41,34 @@ class CouponMongoRepo(CouponRepository):
             raise HTTPException(status_code=500, detail="Failed to create coupon")
 
     def getCoupon(self, couponId: str) -> CouponItem:
+        """
+        Get a coupon item by id
+
+        Args:
+            couponId (str): Coupon id
+
+        Returns:
+            CouponItem: Coupon item
+        """
         coupon = self._collection.find_one({"id": couponId})
         if coupon:
             return CouponItem(**coupon)
         else:
-            raise HTTPException(status_code=404, detail="Coupon not found")
+            return None
 
     def updateCoupon(self, couponItem: CouponItem) -> CouponItem:
+        """
+        Update a coupon item
+
+        Args:
+            couponItem (CouponItem): Coupon item to be updated
+
+        Raises:
+            HTTPException: Failed to update coupon
+
+        Returns:
+            CouponItem: Updated coupon item
+        """
         self._collection.update_one({"id": couponItem.id}, {"$set": couponItem.model_dump()})
 
         coupon = self._collection.find_one({"id": couponItem.id})
@@ -41,5 +78,14 @@ class CouponMongoRepo(CouponRepository):
             raise HTTPException(status_code=500, detail="Failed to update coupon")
 
     def deleteCoupon(self, couponId: str) -> bool:
+        """
+        Delete a coupon item by id
+
+        Args:
+            couponId (str): Coupon id
+
+        Returns:
+            bool: True if coupon is deleted, False otherwise
+        """
         result = self._collection.delete_one({"id": couponId})
         return result.deleted_count > 0
