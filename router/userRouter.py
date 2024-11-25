0c414c38d64a59cf45b8ea2fdae8248d4ba57dfc
DrCloy <alsry0712@gmail.com>
2024-11-25 14:06:16 +0000

    Add docstring

--- router/userRouter.py
+++ router/userRouter.py
@@ -5,16 +5,37 @@ from core.repo import UserRepository
 
 
 class UserRouter(APIRouter):
+    """
+    UserRouter class
+
+    This class is a router class for user-related API endpoints.
+    """
+
     def __init__(self, userRepo: UserRepository):
         super().__init__(prefix="/user")
         self._userRepo = userRepo
 
-        self.add_api_route(path="/register", endpoint=self.register, methods=["POST"])
-        self.add_api_route(path="/profile/{userId}", endpoint=self.getProfile, methods=["GET"])
-        self.add_api_route(path="/profile", endpoint=self.updateProfile, methods=["PUT"])
-        self.add_api_route(path="/delete/{userId}", endpoint=self.deleteUser, methods=["DELETE"])
+        self.add_api_route(path="/register", endpoint=self._register, methods=["POST"])
+        self.add_api_route(path="/profile/{userId}", endpoint=self._getProfile, methods=["GET"])
+        self.add_api_route(path="/profile", endpoint=self._updateProfile, methods=["PUT"])
+        self.add_api_route(path="/delete/{userId}", endpoint=self._deleteUser, methods=["DELETE"])
 
-    def register(self, userItem: UserItem, request: Request) -> UserItem:
+    def _register(self, userItem: UserItem, request: Request) -> UserItem:
+        """
+        Register a new user
+        This method registers a new user with the given userItem.
+
+        Args:
+            userItem (UserItem): The userItem to register
+            request (Request): The request object
+
+        Raises:
+            HTTPException(status_code=401): If the user is not authenticated
+            HTTPException(status_code=409): If the user already exists
+
+        Returns:
+            UserItem: The registered userItem
+        """
         if request.state.auth is None:
             raise HTTPException(status_code=401, detail="Unauthorized")
 
@@ -27,10 +48,29 @@ class UserRouter(APIRouter):
 
         return user
 
-    def getProfile(self, userId: str, request: Request) -> UserItem:
+    def _getProfile(self, userId: str, request: Request) -> UserItem:
+        """
+        Get the user profile with userId
+
+        This method gets the user profile with the given userId.
+        If the user is not authenticated, it will return a 401 error.
+        If the user is not found, it will return a 404 error.
+
+        Args:
+            userId (str): The userId to get the profile
+            request (Request): The request object
+
+        Raises:
+            HTTPException(status_code=401): If the user is not authenticated
+            HTTPException(status_code=404): If the user is not found
+
+        Returns:
+            UserItem: The user profile
+        """
         if request.state.auth is None:
             raise HTTPException(status_code=401, detail="Unauthorized")
-
+        if not request.state.auth.get("sub"):
+            raise HTTPException(status_code=401, detail="Unauthorized")
         if request.state.auth["sub"] != userId:
             raise HTTPException(status_code=401, detail="Unauthorized")
 
@@ -41,35 +81,72 @@ class UserRouter(APIRouter):
 
         return user
 
-    # TODO: 사용자의 권한으로는 수정할 수 없는 정보들에 대해서 어떻게 수정할 지 해결해야 함(point)
-    def updateProfile(self, userItem: UserItem, request: Request) -> UserItem:
+    def _updateProfile(self, userItem: UserItem, request: Request) -> UserItem:
+        """
+        Update the user profile with userItem
+        This method updates the user profile with the given userItem.
+
+        Args:
+            userItem (UserItem): The userItem to update
+            request (Request): The request object
+
+        Raises:
+            HTTPException(status_code=400): If the userItem is invalid
+            HTTPException(status_code=401): If the user is not authenticated
+            HTTPException(status_code=404): If the user is not found
+
+        Returns:
+            UserItem: The updated user profile
+        """
         if request.state.auth is None:
             raise HTTPException(status_code=401, detail="Unauthorized")
+        if not request.state.auth.get("sub"):
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-        user = self.getProfile(request.state.auth["sub"], request)
+        user = self._getProfile(request.state.auth["sub"], request)
         if not user:
             raise HTTPException(status_code=404, detail="User not found")
 
         if not user.couponList == userItem.couponList:
-            raise HTTPException(status_code=403, detail="Unauthorized")
+            raise HTTPException(status_code=400, detail="Bad Request")
         if not user.challengeList == userItem.challengeList:
-            raise HTTPException(status_code=403, detail="Unauthorized")
+            raise HTTPException(status_code=400, detail="Bad Request")
 
         user = self._userRepo.updateUser(userItem)
 
         return user
 
-    def deleteUser(self, userId: str, request: Request) -> bool:
+    # TODO: Implement the following methods
+    def _addPoint(self, request: Request) -> UserItem:
+        """
+        Add point to the user
+        This route is callback route for Google AdMob reward video ad.
+        """
+        pass
+
+    def _deleteUser(self, userId: str, request: Request) -> bool:
+        """
+        Delete the user with userId
+        This method deletes the user with the given userId.
+
+        Args:
+            userId (str): The userId to delete
+            request (Request): The request object
+
+        Raises:
+            HTTPException(status_code=401): If the user is not authenticated
+            HTTPException(status_code=404): If the user is not found
+
+        Returns:
+            bool: True if the user is successfully deleted, False otherwise
+        """
         if request.state.auth is None:
             raise HTTPException(status_code=401, detail="Unauthorized")
 
-        if request.state.auth["role"] == "test":
-            pass
-        else:
-            if not self.getProfile(request.state.auth["sub"], request):
-                raise HTTPException(status_code=404, detail="User not found")
-            if request.state.auth["sub"] != userId:
-                raise HTTPException(status_code=401, detail="Unauthorized")
+        if not self._getProfile(request.state.auth["sub"], request):
+            raise HTTPException(status_code=404, detail="User not found")
+        if request.state.auth["sub"] != userId:
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
         result = self._userRepo.deleteUser(userId)
 
