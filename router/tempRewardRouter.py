5aca424a46d0f62ffa851c3cb0cff5b49affa761
DrCloy <alsry0712@gmail.com>
2024-11-26 07:00:35 +0000

    Update reward router

--- router/tempRewardRouter.py
+++ router/tempRewardRouter.py
@@ -1,8 +1,10 @@
 import os
+import io
+import base64
 import random
 import datetime
 
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Request, UploadFile
 
 from core.model import RewardItem, RewardItemMeta, CouponItem, CouponItemMeta, FileData
 from core.repo import UserRepository, RewardRepository, CouponRepository, FileRepository
@@ -28,8 +30,8 @@ class RewardRouter(APIRouter):
         self._couponRepo = couponRepo
         self._fileRepo = fileRepo
 
-        self.add_api_route(methods=["POST"], path="/create", endpoint=self.createReward)
-        self.add_api_route(methods=["GET"], path="/all", endpoint=self.getAllRewards)
+        self.add_api_route(methods=["POST"], path="/create", endpoint=self._createReward)
+        self.add_api_route(methods=["GET"], path="/all", endpoint=self._getAllRewards)
         self.add_api_route(methods=["GET"], path="/{rewardId}", endpoint=self.getReward)
         self.add_api_route(methods=["PUT"], path="/update", endpoint=self.updateReward)
         self.add_api_route(methods=["DELETE"], path="/delete/{rewardId}", endpoint=self.deleteReward)
@@ -37,7 +39,7 @@ class RewardRouter(APIRouter):
         self.add_api_route(methods=["POST"], path="/extend/{couponId}", endpoint=self.extendExpiration)
         self.add_api_route(methods=["DELETE"], path="/delete/{couponId}", endpoint=self.deleteCoupon)
 
-    def createReward(self, rewardItem: RewardItem, request: Request) -> RewardItem:
+    def _createReward(self, rewardItem: RewardItem, request: Request) -> RewardItem:
         """
         Create a new reward item
 
@@ -56,10 +58,13 @@ class RewardRouter(APIRouter):
         if not request.state.auth.get("sub"):
             raise HTTPException(status_code=401, detail="Unauthorized")
 
+        userId = request.state.auth.get("sub")
+        # TODO: Check if the user is an admin
+
         reward = self._rewardRepo.createReward(rewardItem)
         return reward
 
-    def getAllRewards(self) -> list[RewardItemMeta]:
+    def _getAllRewards(self) -> list[RewardItemMeta]:
         """
         Get all rewards
 
@@ -70,7 +75,7 @@ class RewardRouter(APIRouter):
 
     def getReward(self, rewardId: str) -> RewardItem:
         """
-        Get a reward by rewardId
+        Get a reward data by rewardId
 
         Args:
             rewardId (str): The rewardId to get
@@ -101,9 +106,7 @@ class RewardRouter(APIRouter):
             raise HTTPException(status_code=401, detail="Unauthorized")
 
         userId = request.state.auth.get("sub")
-        user = self._userRepo.getUser(userId)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
+        # TODO: Check if the user is an admin
 
         reward = self._rewardRepo.updateReward(rewardItem)
         return reward
@@ -128,9 +131,7 @@ class RewardRouter(APIRouter):
             raise HTTPException(status_code=401, detail="Unauthorized")
 
         userId = request.state.auth.get("sub")
-        user = self._userRepo.getUser(userId)
-        if not user:
-            raise HTTPException(status_code=404, detail="User not found")
+        # TODO: Check if the user is an admin
 
         return self._rewardRepo.deleteReward(rewardId)
 
@@ -151,12 +152,18 @@ class RewardRouter(APIRouter):
         """
         if not request.state.auth:
             raise HTTPException(status_code=401, detail="Unauthorized")
+        if not request.state.auth.get("sub"):
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-        user = self._userRepo.getUser(request.state.auth.get("sub"))
+        userId = request.state.auth.get("sub")
+        if not self._userRepo.getUser(userId):
+            raise HTTPException(status_code=404, detail="User not found")
+
+        user = self._userRepo.getUser(userId)
+
         reward = self._rewardRepo.getReward(rewardId)
         if user.point < reward.point:
             raise HTTPException(status_code=400, detail="Not enough point")
-        user.point -= reward.point
 
         if reward.itemType == '햄버거':
             couponId = random.choice(coupon_bugger)
@@ -164,21 +171,36 @@ class RewardRouter(APIRouter):
             couponId = random.choice(coupon_chicken)
         elif reward.itemType == '커피':
             couponId = random.choice(coupon_coffee)
+        try:
+            couponImage = self._fileRepo.getFile(couponId)
+            imageFile = io.BytesIO(base64.b64decode(couponImage.file))
+            newCouponImage = self._fileRepo.createFile(
+                file=UploadFile(
+                    file=imageFile,
+                    size=couponImage.size,
+                    filename=couponImage.name,
+                    content_type=couponImage.contentType
+                ),
+                userId=userId,
+                isPrivate=True)
 
-        coupon = CouponItem(
-            itemName=reward.itemName,
-            brandName=reward.brandName,
-            description=reward.description,
-            thumbnailId=couponId,
-            couponId=couponId,
-            expiredAt=str(int((datetime.datetime.now() + datetime.timedelta(days=7)).timestamp()))
-        )
-        coupon = self._couponRepo.createCoupon(coupon)
-        user.couponList.append(CouponItemMeta(**coupon))
+            coupon = CouponItem(
+                itemName=reward.itemName,
+                brandName=reward.brandName,
+                description=reward.description,
+                thumbnailId=couponId,
+                couponId=newCouponImage.id,
+                expiredAt=str(int((datetime.datetime.now() + datetime.timedelta(days=7)).timestamp()))
+            )
+            coupon = self._couponRepo.createCoupon(coupon)
 
-        user = self._userRepo.updateUser(user)
+            user.couponList.append(CouponItemMeta(**coupon))
+            user.point -= reward.point
+            user = self._userRepo.updateUser(user)
 
-        return coupon
+            return coupon
+        except Exception as e:
+            raise HTTPException(status_code=500, detail="Failed to purchase reward, " + str(e))
 
     def extendExpiration(self, couponId: str, request: Request) -> CouponItem:
         """
@@ -191,17 +213,25 @@ class RewardRouter(APIRouter):
         Raises:
             HTTPException(status_code=400): If the coupon is not found
             HTTPException(status_code=401): If the user is not authorized
+            HTTPException(status_code=404): If the user is not found
 
         Returns:
             CouponItem: The extended coupon item
         """
         if not request.state.auth:
             raise HTTPException(status_code=401, detail="Unauthorized")
+        if not request.state.auth.get("sub"):
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-        user = self._userRepo.getUser(request.state.auth.get("sub"))
+        userId = request.state.auth.get("sub")
+
+        user = self._userRepo.getUser(userId)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
 
         if not next((coupon for coupon in user.couponList if coupon.couponId == couponId), None):
             raise HTTPException(status_code=400, detail="Coupon not found")
+
         coupon = self._couponRepo.getCoupon(couponId)
         coupon.expiredAt = str(int((datetime.datetime.fromtimestamp(int(coupon.expiredAt)) + datetime.timedelta(days=7)).timestamp()))
         coupon = self._couponRepo.updateCoupon(coupon)
@@ -219,14 +249,20 @@ class RewardRouter(APIRouter):
         Raises:
             HTTPException(status_code=400): If the coupon is not found
             HTTPException(status_code=401): If the user is not authorized
+            HTTPException(status_code=404): If the user is not found
 
         Returns:
             bool: True if the coupon is deleted, False otherwise
         """
         if not request.state.auth:
             raise HTTPException(status_code=401, detail="Unauthorized")
+        if not request.state.auth.get("sub"):
+            raise HTTPException(status_code=401, detail="Unauthorized")
 
-        user = self._userRepo.getUser(request.state.auth.get("sub"))
+        userId = request.state.auth.get("sub")
+        user = self._userRepo.getUser(userId)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
 
         if not next((coupon for coupon in user.couponList if coupon.couponId == couponId), None):
             raise HTTPException(status_code=400, detail="Coupon not found")
