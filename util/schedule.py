23850f98de7b7d54b08c43f7a68fdce6f295b39b
DrCloy <alsry0712@gmail.com>
2024-12-12 05:09:58 +0000

    Update scheduler method

--- util/schedule.py
+++ util/schedule.py
@@ -1,8 +1,22 @@
-def check_ad_log():
-    # TODO: Implement this function
-    pass
+from datetime import datetime
+
+from core.model import ItemState
+from core.repo import ChallengeRepository
+from util.adVerifier import AdVerifier
 
 
-def check_challenge_expiry():
-    # TODO: Implement this function
-    pass
+async def check_ad_log(adVerifier: AdVerifier):
+    await adVerifier.remove_old_log()
+
+    return
+
+
+def check_challenge_expiry(challengeRepository: ChallengeRepository):
+    challenges = challengeRepository.getAllChallenges()
+    for challenge in challenges:
+        if int(challenge.dateEnd) < datetime.now().timestamp():
+            challenge_detail = challengeRepository.getChallenge(challenge.id)
+            challenge_detail.state = ItemState.FINISHED
+            challengeRepository.updateChallenge(challenge_detail)
+
+    return
